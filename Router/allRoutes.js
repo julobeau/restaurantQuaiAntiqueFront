e7dee65e1b100e29f70eb09b96c99d6712566c08
julobeau <julien.beaumel@free.fr>
2024-03-03 18:59:44 +0100

    galerie, carte, connexion, inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,8 +3,12 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Galerie", "Galerie", "/pages/galerie.html", "./js/galerie.js"),
-];
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/carte", "Carte et menus", "/pages/carte.html"),
+    new Route("/connexion", "Connexion", "/pages/auth/connexion.html"),
+    new Route("/inscription", "Inscription", "/pages/auth/inscription.html"),
+    new Route("/account", "Compte", "/pages/auth/account.html"),
+ ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
