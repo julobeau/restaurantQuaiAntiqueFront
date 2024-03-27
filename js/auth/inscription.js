d01bc00ea3876701babd372dfdb3893ca1fcd6fe
julobeau <julien.beaumel@free.fr>
2024-03-27 14:11:45 +0100

    mise en place api +  inscription

--- js/auth/inscription.js
+++ js/auth/inscription.js
@@ -1,3 +1,12 @@
+const btnValidation = document.getElementById("btn-validation-inscription");
+const inputNom = document.getElementById("NomInput");
+const inputPrenom = document.getElementById("PrenomInput");
+const inputMail = document.getElementById("EmailInput");
+const inputPassword = document.getElementById("passwordInput");
+const inputvalidationPassword = document.getElementById("passwordValid");
+const formInscription = document.getElementById("formulaireInscription")
+btnValidation.disabled = true
+
 // validation champs inscription
 
 const validateForm = () => {
@@ -71,17 +80,48 @@ const validateValidationPassword = (validatePassword, password) => {
     }
 }
 
+const inscrireUtilisateur = () => {
+    let dataForm = new FormData(formInscription)
 
-const btnValidation = document.getElementById("btn-validation-inscription");
-const inputNom = document.getElementById("NomInput");
-const inputPrenom = document.getElementById("PrenomInput");
-const inputMail = document.getElementById("EmailInput");
-const inputPassword = document.getElementById("passwordInput");
-const inputvalidationPassword = document.getElementById("passwordValid");
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({
+        "firstName": dataForm.get("prenom"),
+        "lastName": dataForm.get("nom"),
+        "email": dataForm.get("email"),
+        "password": dataForm.get("password")
+    });
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("http://127.0.0.1:8000/api/registration", requestOptions)
+    .then(response => {
+        if(response.ok){
+            return response.json()
+        }
+        else{
+            alert("Erreur lors de l'inscription")
+        }
+    })
+    .then(result => {
+        alert("Vous êtes inscrit!! Vous pouvez vous connecter.")
+        document.location.href="/connexion"
+    })
+    
+    .catch(error => console.log('error', error));
+} 
+
+
 
 inputNom.addEventListener("keyup", validateForm);
 inputPrenom.addEventListener("keyup", validateForm);
 inputMail.addEventListener("keyup", validateForm);
 inputPassword.addEventListener("keyup", validateForm);
 inputvalidationPassword.addEventListener("keyup", validateForm);
-
+btnValidation.addEventListener("click", inscrireUtilisateur)
